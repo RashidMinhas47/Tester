b860498d45ad862fd8a97d1b3ea2040176b7d243
asc <rashidminhassheikh47@gmail.com>
2025-01-21 21:19:27 +0500

    Base Code

---
+++ android/app/src/main/kotlin/com/example/flow_of_github/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flow_of_github
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
