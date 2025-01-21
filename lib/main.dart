d9b184b7c41c1be4550f506ca15b044544bdf6cc
CodeXa-Soft <quickenglishbites@gmail.com>
2025-01-21 22:47:58 +0500

    Home Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -25,3 +25,12 @@ class App extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen` extends StatelessWidget {
+  const HomeScreen`({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
